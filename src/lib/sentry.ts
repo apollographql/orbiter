fe2669232089d94987e211c961246a05f82a8d40
Avery Harnish <averyharnish@gmail.com>
2021-05-05 11:00:02 -0500

    fix: _really_ ignore SENTRY_DSN locally

--- src/lib/sentry.ts
+++ src/lib/sentry.ts
@@ -1,8 +1,17 @@
 // https://docs.sentry.io/platforms/node/guides/aws-lambda/
 import { AWSLambda } from '@sentry/serverless';
+import { Handler } from "aws-lambda";
 
 export function initSentry() {
   AWSLambda.init();
 }
 
-export const sentryWrapHandler = AWSLambda.wrapHandler;
+function wrapHandler<TEvent, TResult>(handler: Handler<TEvent, TResult>, wrapOptions?: Partial<AWSLambda.WrapperOptions> | undefined): Handler<TEvent, TResult | undefined> {
+  if (process.env.SENTRY_DSN) {
+    return AWSLambda.wrapHandler(handler, wrapOptions)
+  } else {
+    return handler
+  }
+}
+
+export const sentryWrapHandler = wrapHandler;
