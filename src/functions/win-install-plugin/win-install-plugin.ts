31e9828168e552705eb7cd0389c6a8cac3011104
Avery Harnish <averyharnish@gmail.com>
2021-10-28 11:28:16 -0500

    feat: adds plugin installers

---
+++ src/functions/win-install-plugin/win-install-plugin.ts
@@ -0,0 +1,74 @@
+import {
+  Handler,
+  APIGatewayProxyResult,
+  APIGatewayProxyEvent,
+} from 'aws-lambda';
+import { getFetcher } from '../../lib/getFetcher';
+import { track } from '../../lib/segment';
+import { getPluginFromEvent } from '../../lib/plugin';
+import { initSentry, sentryWrapHandler } from '../../lib/sentry';
+
+initSentry();
+
+const handler: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> = async (
+  event,
+) => {
+  const fetch = getFetcher();
+  let plugin: [string, string];
+  try {
+    plugin = await getPluginFromEvent(event);
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: e.message,
+    };
+  }
+
+  let plugin_name = plugin[0];
+  let plugin_version = plugin[1];
+
+  let response = await fetch(
+    `https://raw.githubusercontent.com/apollographql/rover/${plugin_version}/installers/binstall/scripts/windows/install_${plugin_name}.ps1`,
+  );
+
+  if(response.ok) {
+    const winInstallScript: string = await response.text();
+
+    // Track the download, but explicitly _don't_ block on it
+    track({
+      event: `${plugin_name} Download`,
+      context: {
+        app: 'Rover',
+        os: 'windows',
+      },
+      properties: {
+        release_version: plugin_version,
+      },
+    });
+
+    return {
+      statusCode: 200,
+      body: winInstallScript,
+      headers: {
+        "X-Version": plugin_version
+      }
+    };
+  }
+
+  if (response.status === 404){
+    return {
+      statusCode: 400,
+      body: `Couldn't find release for ${plugin_name}@${plugin_version} on GitHub Releases. This could be a problem with GitHub being offline or missing this version`,
+    }
+  }
+
+  return {
+    statusCode: 500,
+    body: `Error when loading Rover installer for ${plugin_name}@${plugin_version} from GitHub releases. This could be because GitHub is down. The error we received from GitHub was ${response.statusText}`
+  }
+};
+
+module.exports = {
+  handler: sentryWrapHandler(handler),
+  unwrappedHandler: handler,
+};
---
+++ src/lib/plugin.ts
@@ -0,0 +1,40 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { getLatestVersion } from './version';
+
+export const LATEST_URL = 'https://github.com/apollographql/rover/releases/latest';
+
+export async function getPluginFromEvent(event: APIGatewayProxyEvent): Promise<[string, string]> {
+  // /plugins/rover-fed2/nix/v0.0.2
+  // @ts-ignore TS6133
+  const [_, __, plugin_name, platform, version] = event.path.split('/');
+
+  const supportedPlugins = ['rover-fed2'];
+  if (!plugin_name) {
+    throw new Error(`No plugin provided. Must be one of: ${supportedPlugins}`)
+  }
+
+  const supportedPlatforms = ['nix', 'win'];
+  if (!platform) {
+    throw new Error(`No platform provided. Must include one of: ${supportedPlatforms}`)
+  }
+
+  if (!version) {
+    throw new Error('No version provided. Versions must be `latest` or in semver format `v0.0.0`')
+  }
+
+  // check for unsupported platform
+  if (!supportedPlatforms.includes(platform.toLowerCase())) {
+    throw new Error(`Invalid platform: ${platform}. Supported platforms: ${supportedPlatforms}`);
+  }
+
+  // check for 'latest' or properly formatted version id
+  if (version.toLowerCase() == 'latest'){
+    const latestVersion = await getLatestVersion();
+    return [plugin_name, latestVersion];
+  }
+  else if (version.toLowerCase().startsWith('v')) {
+    return [plugin_name, version];
+  } else {
+    throw new Error(`Invalid version: (${version}). Versions must start with a 'v' or be 'latest'.`)
+  }
+}
