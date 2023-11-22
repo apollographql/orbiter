4ad21eec672ffd796700c273146351db82623cb7
Jeremy Lempereur <jeremy.lempereur@iomentum.com>
2023-11-22 12:46:29 +0100

    URGENT: Mitigate rover dev outage: router tarball download url on x86_64 linux (#91)
    ---------
    
    Co-authored-by: Jesse Rosenberger <git@jro.cc>

--- src/lib/download.ts
+++ src/lib/download.ts
@@ -38,8 +38,14 @@ export async function downloadEvent(
         "You must either download a tarball or an install script"
       );
     }
-    let response = await fetch(endpoint, { method: "HEAD" });
-    if (response.ok) {
+    // In order to mitigate an outage, we no longer follow redirects here (which is the m-f-h default)
+    // but instead accept that we **may** get a redirect.
+    // Slack: https://apollograph.slack.com/archives/C04933VF6CV/p1700645363560329
+    let response = await fetch(endpoint, {
+      method: "HEAD",
+      redirect: "manual",
+    });
+    if (response.ok || response.status === 302) {
       return {
         statusCode: 302,
         body: `You are being redirected to ${endpoint}`,
